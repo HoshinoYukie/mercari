631f10cf4002b2849595b051954f75c01025620e
HoshinoYukie <2>
2023-04-30 18:58:50 +0800

    "httpSecurity"

---
+++ demo/src/main/java/com/example/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                //权限认证
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/api/auth/login")
+                .and()
+                .logout()
+                .logoutUrl("/api/auth/logout")
+                .and()
+                //关闭 csrf
+                .csrf()
+                .disable()
+                .build();
+    }
+}
