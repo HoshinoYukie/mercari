fed2dc2266fbcad2e2af0549a2448b31ac374726
HoshinoYukie <2>
2023-05-01 18:15:52 +0800

    "httpSecurity"

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class DemoApplication {
